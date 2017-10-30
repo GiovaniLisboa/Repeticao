7454c867c0365804ea69b63bc3802c3435e47b6f
Giovani Lisboa <giovanilisboa94@gmail.com>
2017-10-30 19:42:34 -0300

    While

---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include <iostream>
+using std::cout;
+using std::cin;
+
+int main(int argc, char **argv)
+{
+    int bomba = 10;
+    char teste;
+    
+    while (true)
+    {
+        cout << "Contagem regressiva para a explosao: ";
+        cout << bomba;
+        cout << "\n";
+        
+        if (bomba == 0)
+            {
+                cout << "Cabum! \n \n";
+                break;
+            }
+            
+        bomba -= 1;
+    }
+    
+    bomba = 10;
+    
+    do
+    {
+        cout << "Contagem regressiva para a explosao: ";
+        cout << bomba;
+        cout << "\n";
+        
+        if (bomba == 0)
+        {
+            cout << "Cabum!\n";
+        }
+        
+        if (bomba == 1 && teste != 's' && teste != 'n')
+        {
+            while (teste != 's' && teste != 'n')
+            {
+                cout << "Deseja desarmar a bomba? (S/N) ";
+                cin >> teste;
+            }
+            if (teste == 's')
+            {
+                cout << "Bomba desarmada.";
+            }
+            else
+            {
+                cout << "Bomba\n";
+                bomba--;
+                cout << bomba << '\n';
+                continue;
+            }
+        }
+        bomba -= 1;
+        
+    } while (bomba > 0);
+    
+    cout << "\n";
+    return 0;
+}
